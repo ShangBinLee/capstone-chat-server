663f9b8336e5aedc0f165e686754835f2569840d
ShangBin <dkfkgks64@gmail.com>
2023-05-29 04:33:27 +0900

    feature : Add src/DB/select_chat.js, Implement selectChatHistoryAfter
    - close #10.

--- src/DB/select_chat.js
+++ src/DB/select_chat.js
@@ -4,6 +4,16 @@ const selectChatCreateDate = (chatId, pool, query) => {
     return query(pool, sql, chatId);
 };
 
+const selectChatHistoryAfter = (chatRoomId, dateString, pool, query) => {
+    const sql = `SELECT id, chat_content, create_date
+    FROM chat
+    WHERE chat_room_id = ? and create_date > ?
+    ORDER BY create_date ASC`;
+
+    return query(pool, sql, [ chatRoomId, dateString ]);
+};
+
 export {
-    selectChatCreateDate
+    selectChatCreateDate,
+    selectChatHistoryAfter
 };
