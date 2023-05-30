f2ed4cd2692bd2fa7ec07f3b1ddaedf1bd39e773
ShangBin <dkfkgks64@gmail.com>
2023-05-30 23:36:29 +0900

    test : Add test/connection.test.js as integration test
    Implement connection event test.

---
+++ test/connection.test.js
@@ -0,0 +1,48 @@
+import { Server } from 'socket.io';
+import Client from 'socket.io-client';
+import { initializeSocket } from '#src/controller/socket_controller.js';
+
+describe('connection 이벤트 테스트', () => {
+  let io, clientSocket, socketUserMap;
+
+  beforeAll(() => {
+    io = new Server();
+
+    const middlewares = [
+			(socket, next) => {
+				socket.user = { id : 'aifjodsjof123123-123912xkdkdk' };
+				next();
+			}
+    ];
+
+		socketUserMap = new Map();
+
+    initializeSocket(io, middlewares, socketUserMap);
+
+    io.listen(3000);
+  });
+
+	afterEach(() => {
+		clientSocket.close();
+	});
+
+  afterAll(() => {
+    io.close();
+  });
+
+  test('클라이언트 connection 이벤트 수신', (done) => {
+    clientSocket = new Client(`http://localhost:${3000}`);
+    clientSocket.on("connection", (msg) => {
+			expect(msg).toStrictEqual({ status : 'connected' });
+			done();
+		});
+  });
+
+	test('socketUserMap 올바른 키-값 저장', (done) => {
+		clientSocket = new Client(`http://localhost:${3000}`);
+    clientSocket.on("connection", (msg) => {
+			expect(socketUserMap.get(clientSocket.id)).toBe('aifjodsjof123123-123912xkdkdk');
+			done();
+		});
+	})
+});
