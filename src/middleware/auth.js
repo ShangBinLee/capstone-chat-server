9efd285b6f30f3c21b1fe123dcb359fc4bd145d3
ShangBin <dkfkgks64@gmail.com>
2023-05-30 14:59:09 +0900

    feature : Add import, Implement userInfoFetcher - close #14

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,3 +1,5 @@
+import { fetchUserInfo } from '#src/api_client/user/fetch_user.js';
+
 /**
  * socket이 token을 통한 인증으로 서버에 접속하려고 하는지 확인하는 미들웨어
  */
@@ -11,4 +13,22 @@ const tokUseChecker = (socket, next) => {
 	next();
 };
 
-export { tokUseChecker };
+/**
+ * fetch와 rootUrl을 받아 socket의 token으로 유저 정보를 가져오는 미들웨어를 반환한다
+ * @param {Function} fetch - fetch 함수
+ * @param {String} rootUrl - 중앙 서버 api root url
+ * @return {Function} 미들웨어
+ */
+const userInfoFetcher = (fetch, rootUrl) => async (socket, next) => {
+	const tok = socket.handshake.auth.token;
+	const userInfo = await fetchUserInfo(fetch, tok, rootUrl);
+
+	socket.user = userInfo;
+
+	next();
+}
+
+export { 
+	tokUseChecker,
+	userInfoFetcher
+};
