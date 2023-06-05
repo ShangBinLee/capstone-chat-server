b7f71fdcf8cf983c9d3813e04755953cf8fe5f04
LeeShangBin <dkfkgks64@gmail.com>
2023-06-05 16:23:12 +0900

    feature : Implement fetchProduct - close #17

--- src/api_client/product/fetch_product.js
+++ src/api_client/product/fetch_product.js
@@ -1,6 +1,6 @@
 /**
  * 중앙 서버 API를 통해 tok 소유자가 올린 모든 상품 정보를 가져옴
- * @param {function} fetch - fetch API의 fetch 함수
+ * @param {Function} fetch - fetch API의 fetch 함수
  * @param {string} tok - 인증을 위한 JWT token
  * @param {string} rootUrl - 중앙 서버 API의 root URL
  */
@@ -8,7 +8,27 @@ const fetchSellerProductsAll = (fetch, tok, rootUrl) => {
 	const headers = { Authentication : `berear ${tok}` };
 
 	return fetch(`${rootUrl}/saleproduct/find/my/product`, { headers })
-		.then(res => res.json());
+		.then((res) => res.json());
 };
 
-export { fetchSellerProductsAll };
+/**
+ * 중앙 서버 API를 통해 productId에 해당하는 상품 정보를 가져옴
+ * @param {Function} fetch - fetch API의 fetch 함수
+ * @param {Number} productId - 조회할 상품 id
+ * @param {string} rootUrl - 중앙 서버 API의 root URL
+ */
+const fetchProduct = (fetch, productId, rootUrl) => {
+	const params = {
+		field : 'all'
+	};
+
+	const querystring = new URLSearchParams(params).toString();
+
+	return fetch(`${rootUrl}/chat/saleproduct/${productId}?${querystring}`)
+		.then((res) => res.json());
+};
+
+export {
+  fetchSellerProductsAll,
+  fetchProduct
+};
