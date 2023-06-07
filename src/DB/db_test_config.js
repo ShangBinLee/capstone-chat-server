91d6874f53f5ef0549da97e9a4198547f320be25
ShangBin <dkfkgks64@gmail.com>
2023-06-08 01:17:08 +0900

    chore : Add src/DB/db_test_config.js to test testDB

---
+++ src/DB/db_test_config.js
@@ -0,0 +1,9 @@
+export default {
+	host : 'localhost',
+	user : 'bit_chat_test',
+	password : '1234',
+	database : 'bit_chat_test',
+	port : 3306,
+	enableKeepAlive : true,
+	dateStrings : 'date'
+};
