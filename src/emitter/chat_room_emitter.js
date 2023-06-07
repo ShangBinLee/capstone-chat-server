df2a619d693cace38adc07a1bf61627dc53a0c1e
ShangBin <dkfkgks64@gmail.com>
2023-06-07 23:16:04 +0900

    feature : Implement notificateNewChatRoomEmitter, Add export - close #34

--- src/emitter/chat_room_emitter.js
+++ src/emitter/chat_room_emitter.js
@@ -1,3 +1,12 @@
 const getChatRoomsEmitter = (socket, chatRooms) => {
   socket.emit('get_chat_rooms', chatRooms);
 };
+
+const notificateNewChatRoomEmitter = (socket, rooms, msg) => {
+  socket.to(rooms).emit('notificate_new_chat_room', msg);
+};
+
+export {
+  getChatRoomsEmitter,
+  notificateNewChatRoomEmitter
+};
