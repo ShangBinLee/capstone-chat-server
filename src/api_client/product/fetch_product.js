5487a3b3da83a748a5feb7a5737a4be80fcc7d67
ShangBin <dkfkgks64@gmail.com>
2023-06-10 02:11:43 +0900

    update : Remove fetchSellerProductsAll

--- src/api_client/product/fetch_product.js
+++ src/api_client/product/fetch_product.js
@@ -1,17 +1,4 @@
 /**
- * 중앙 서버 API를 통해 tok 소유자가 올린 모든 상품 정보를 가져옴
- * @param {Function} fetch - fetch API의 fetch 함수
- * @param {string} tok - 인증을 위한 JWT token
- * @param {string} rootUrl - 중앙 서버 API의 root URL
- */
-const fetchSellerProductsAll = (fetch, tok, rootUrl) => {
-	const headers = { Authentication : `berear ${tok}` };
-
-	return fetch(`${rootUrl}/saleproduct/find/my/product`, { headers })
-		.then((res) => res.json());
-};
-
-/**
  * 중앙 서버 API를 통해 productId에 해당하는 상품 정보를 가져옴
  * @param {Function} fetch - fetch API의 fetch 함수
  * @param {Number} productId - 조회할 상품 id
@@ -23,6 +10,5 @@ const fetchProduct = (fetch, productId, rootUrl) => {
 };
 
 export {
-  fetchSellerProductsAll,
   fetchProduct
 };
