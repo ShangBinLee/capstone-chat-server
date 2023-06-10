532e0402453361ca85e1cc4e422198730ed0fc70
ShangBin <dkfkgks64@gmail.com>
2023-06-11 05:04:10 +0900

    fix : Fix wrong key in HTTP headers in fetchUserInfo
    Replace tok parameter with authorization.

--- src/api_client/user/fetch_user.js
+++ src/api_client/user/fetch_user.js
@@ -1,11 +1,11 @@
 /**
  * 중앙 서버 API를 통해 tok 소유자의 유저 정보를 가져옴
  * @param {function} fetch - fetch API의 fetch 함수
- * @param {string} tok - 인증을 위한 JWT token
+ * @param {string} authorization - HTTP headers의 Authorization에 해당하는 값
  * @param {string} rootUrl - 중앙 서버 API의 root URL
  */
-const fetchUserInfo = (fetch, tok, rootUrl) => {
-	const headers = { Authentication : `berear ${tok}` };
+const fetchUserInfo = (fetch, authorization, rootUrl) => {
+	const headers = { 'Authorization' : authorization };
 
 	return fetch(`${rootUrl}/api/user/info`, { headers })
 		.then((res) => res.json());
